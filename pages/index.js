5a4b4d4b057378d709c4351a835dad6c6c3e2131
war <war@warm.local>
2023-08-28 16:21:43 +0200

    alternate way to say the same thing

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ export default function Home() {
   const [searchstring, setSearchstring] = useState("");
   const router = useRouter();
 
-  const onClickRow = (item) => {
+//const onClickRow = (item) => {
+//  router.push(`confirm?id=${item.id}`);
+//};
+
+  const onClickRow = function(item) {
     router.push(`confirm?id=${item.id}`);
-  };
+  }
 
   return (<div>
     <h4>Secure Polling Demo</h4>
