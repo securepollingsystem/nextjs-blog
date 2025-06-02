f557d595451be7e090fdd2c3a851ed5bab2e6191
Charles Hughes <cjhughes87@pm.me>
2025-06-02 15:47:40 -0700

    start vanilla transition

---
+++ components/HelloWorld.js
@@ -0,0 +1,9 @@
+
+class HelloWorldComponent extends HTMLElement {
+  connectedCallback() {
+    this.textContent = 'hello world!';
+  }
+}
+
+customElements.define('x-hello-world', HelloWorldComponent);
+
---
+++ pages/vanilla.js
@@ -0,0 +1,8 @@
+import v from "../components/HelloWorld"
+
+const Vanilla = () => (
+  <x-hello-world></x-hello-world>
+)
+
+export default Vanilla;
+
